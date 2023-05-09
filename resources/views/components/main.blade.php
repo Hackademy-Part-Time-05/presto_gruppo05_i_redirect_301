169e4d2adbbc903b2906b55e6f6348300c95c9d5
max5oooooo <maxuel5oooooo@gmail.com>
2023-05-09 13:09:34 +0200

    added component main and footer

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -8,27 +8,9 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
   <body>
-    <!-- NAVBAR -->
-    <nav class="navbar navbar-expand-lg navbar-scroll fixed-top shadow-0 border-bottom border-dark">
-        <div class="container">
-          <a class="navbar-brand" href="#!">Presto.it</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="#!">Accedi</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#!">Registrati</a>
-              </li>
-              <button type="button" class="btn btn-warning ms-3">+ INSERISCI ANNUNCIO</button>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <x-nav/>
+      
+    <x-footer/>
+      <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   </body>
 </html>
---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-scroll fixed-top shadow-0 border-bottom border-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#!">Presto.it</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Accedi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Registrati</a>
+          </li>
+          <button type="button" class="btn btn-warning ms-3">+ INSERISCI ANNUNCIO</button>
+        </ul>
+      </div>
+    </div>
+  </nav>
