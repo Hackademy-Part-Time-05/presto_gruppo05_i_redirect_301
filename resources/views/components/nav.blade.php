933719d38e4c0a2c25d2ab3a6d764369bdd4d8f4
max5oooooo <maxuel5oooooo@gmail.com>
2023-05-09 14:14:49 +0200

    added views login and register

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-scroll fixed-top shadow-0 border-bottom border-dark">
+<nav class="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
     <div class="container">
       <a class="navbar-brand" href="#!">Presto.it</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,14 +6,29 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="#!">Accedi</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#!">Registrati</a>
-          </li>
-          <button type="button" class="btn btn-warning ms-3">+ INSERISCI ANNUNCIO</button>
-        </ul>
+            @guest
+            <li class="nav-item">
+              <a class="nav-link" href="/register">Registrati</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/login">Accedi</a>
+            </li>
+            @else
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                {{ auth()->user()->email }}
+              </a>
+              <ul class="dropdown-menu dropdown-menu-end">
+                <li>
+                    <form action="/logout" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-sm btn-danger ms-2">Esci</button>
+                    </form>
+                </li>
+                @endguest
+              </ul>
+            </li>
+          </ul>
       </div>
     </div>
   </nav>
