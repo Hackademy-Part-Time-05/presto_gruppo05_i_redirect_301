2ac9c91f258e9369abb49482519888374e9ba4dd
Andrea Luccadi <andrea.gnu.spotify@gmail.com>
2023-05-10 21:30:24 +0200

    fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,4 @@
 <x-main>
-    @foreach ($latestAnnouncements as $latestAnnouncement)
-    <div class="row">
-        <div class="col-6">
-            <x-cards :latestAnnouncement='$latestAnnouncement'/>  
-        </div>
-    </div>
-    @endforeach
 
     <div class="w-100">
         @foreach ($categories as $category)
