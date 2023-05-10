7f3ed30e08a75f9d79ffe9f80f4270b2c3975cad
max5oooooo <maxuel5oooooo@gmail.com>
2023-05-10 20:14:37 +0200

    created card components

---
+++ resources/views/components/cards.blade.php
@@ -0,0 +1,22 @@
+<div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                <a href="">
+                    <div class="card-flyer">
+                        <div class="text-box">
+                            <div class="image-box">
+                                <img src="https://picsum.photos/200/300" alt="" />
+                            </div>
+                            <div class="text-container">
+                                <h6>Macchine</h6>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                                <a href="" class="btn btn-primary shadow">Categoria</a>
+                                <a href="" class="my-2 border-top pt-2 border-dark card-link shadow btn btn-success">Categoria</a>
+                                <p class="card-footer">Pubblicato il:</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
