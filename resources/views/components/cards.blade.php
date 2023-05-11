223997f78b0616631c209fde3671d0f7082fd192
Andrea Luccadi <andrea.gnu.spotify@gmail.com>
2023-05-11 18:59:58 +0200

    fixed card

--- resources/views/components/cards.blade.php
+++ resources/views/components/cards.blade.php
@@ -1,18 +1,15 @@
-<div class="container">
-  <div class="row">
-    <div class="col-12 col-md-4 my-4 pt-5">
-      <div class="card productCard" style="width: 18rem;">
-        <img src="https://picsum.photos/200" class="card-img-top p-3 rounded" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">{{$latestAnnouncement->title}}</h5>
-          <p class="card-text">{{$latestAnnouncement->price}}</p>
-          <p class="card-text">{{$latestAnnouncement->body}}</p>
-          <a href="#" class="btn btn-primary shadow">{{$latestAnnouncement->category->name}}</a><a href="#"
-            class="my-2 border-top pt-2 border-dark card-link shadow btn btn-success">Go somewhere</a>
-          <p>Pubblicato il: {{$latestAnnouncement->created_at}}</p>
-          <p>Caricato da: {{$latestAnnouncement->user}}</p>
-        </div>
-      </div>
+
+<div class="col-12 col-md-4 my-4 pt-5">
+  <div class="card productCard" style="width: 18rem;">
+    <img src="https://picsum.photos/200" class="card-img-top p-3 rounded" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">{{$latestAnnouncement->title}}</h5>
+      <p class="card-text">{{$latestAnnouncement->price}}</p>
+      <p class="card-text">{{$latestAnnouncement->body}}</p>
+      <a href="#" class="btn btn-primary shadow">{{$latestAnnouncement->category->name}}</a><a href="#"
+        class="my-2 border-top pt-2 border-dark card-link shadow btn btn-success">Go somewhere</a>
+      <p>Pubblicato il: {{$latestAnnouncement->created_at}}</p>
+      <p>Caricato da: {{$latestAnnouncement->user->name}}</p>
     </div>
   </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,12 @@
             {{$category}}
         @endforeach
     </div>
-    <div class="w-100">
-        @foreach ($latestAnnouncements as $latestAnnouncement)
-            <x-cards :latestAnnouncement="$latestAnnouncement"></x-cards>
-        @endforeach
+    <div class="w-100 container">
+        <div class="row">
+                @foreach ($latestAnnouncements as $latestAnnouncement)
+                    <x-cards :latestAnnouncement="$latestAnnouncement"></x-cards>
+                @endforeach
+        </div>
     </div>
 
 </x-main>
