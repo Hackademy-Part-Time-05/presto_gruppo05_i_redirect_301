b6bd6cbc3ba89e1b383445b76afb108bb2d6c08a
Andrea Luccadi <andrea.gnu.spotify@gmail.com>
2023-05-11 20:19:14 +0200

    changes footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,58 +1,56 @@
-<footer>
-    <div class="container-fluid pt-5">
-      <footer class="text-center text-lg-start text-dark bg-light pt-2">
-        <section class="">
-          <div class="container text-center text-md-start mt-5">
-            <div class="row mt-3">
-              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                <h6 class="text-uppercase fw-bold">Presto.it</h6>
-                <hr>
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus
-                   magnam error fugiat laborum dicta adipisci, voluptatum,
-                   distinctio quos enim illum aliquid ex, suscipit numquam facilis quaerat omnis odio cum est.
-                </p>
-              </div>
-              <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 class="text-uppercase fw-bold">Prodotti</h6>
-                <hr>
-                <p>
-                  <a href="#!" class="text-dark">lorem</a>
-                </p>
-                <p>
-                  <a href="#!" class="text-dark">lorem</a>
-                </p>
-                <p>
-                  <a href="#!" class="text-dark">lorem</a>
-                </p>
-                <p>
-                  <a href="#!" class="text-dark">lorem</a>
-                </p>
-              </div>
-              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 class="text-uppercase fw-bold">Link utili</h6>
-                <hr>
-                <p>
-                  <a href="#!" class="text-dark">Il tuo account</a>
-                </p>
-                <p>
-                  <a href="#!" class="text-dark">Diventa un Affiliato!</a>
-                </p>
-                <p>
-                  <a href="#!" class="text-dark">Spese di Spedizione</a>
-                </p>
-                <p>
-                  <a href="#!" class="text-dark">Help</a>
-                </p>
-              </div>
-              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                <h6 class="text-uppercase fw-bold">Contattaci</h6>
-                <hr/>
-                <p><i class="fas fa-home mr-3"></i> Italia</p>
-                <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-                <p><i class="fas fa-phone mr-3"></i> 32491043913</p>
-                <p><i class="fas fa-print mr-3"></i>32452312323</p>
-              </div>
-            </div>
-          </div>
-      </footer>
+<footer class="text-center text-lg-start text-dark bg-light pt-2">
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold">Presto.it</h6>
+          <hr>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus
+             magnam error fugiat laborum dicta adipisci, voluptatum,
+             distinctio quos enim illum aliquid ex, suscipit numquam facilis quaerat omnis odio cum est.
+          </p>
+        </div>
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold">Prodotti</h6>
+          <hr>
+          <p class="linkveloci">
+            <a href="#!" class="text-dark nav-link"><i class="fa-solid fa-arrow-right"></i> lorem</a>
+          </p>
+          <p class="linkveloci">
+            <a href="#!" class="text-dark nav-link"><i class="fa-solid fa-arrow-right"></i> lorem</a>
+          </p>
+          <p class="linkveloci">
+            <a href="#!" class="text-dark nav-link"><i class="fa-solid fa-arrow-right"></i> lorem</a>
+          </p>
+          <p class="linkveloci">
+            <a href="#!" class="text-dark nav-link"><i class="fa-solid fa-arrow-right"></i> lorem</a>
+          </p>
+        </div>
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold ">Link utili</h6>
+          <hr>
+          <p class="linkveloci">
+            <a href="#!" class="text-dark nav-link"><i class="fa-solid fa-arrow-right"></i> Il tuo account</a>
+          </p>
+          <p class="linkveloci">
+            <a href="#!" class="text-dark nav-link"><i class="fa-solid fa-arrow-right"></i> Diventa un Affiliato!</a>
+          </p>
+          <p class="linkveloci">
+            <a href="#!" class="text-dark nav-link"><i class="fa-solid fa-arrow-right"></i> Spese di Spedizione</a>
+          </p>
+          <p class="linkveloci">
+            <a href="#!" class="text-dark nav-link"><i class="fa-solid fa-arrow-right"></i> Help</a>
+          </p>
+        </div>
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold">Contattaci</h6>
+          <hr/>
+          <p class="linkveloci"><i class="fas fa-home mr-3"></i> Italia</p>
+          <p class="linkveloci"><i class="fas fa-envelope mr-3"></i> info@example.com</p>
+          <p class="linkveloci"><i class="fas fa-phone mr-3"></i> 32491043913</p>
+          <p class="linkveloci"><i class="fas fa-print mr-3"></i> 32452312323</p>
+        </div>
+      </div>
+    </div>
+</footer>
