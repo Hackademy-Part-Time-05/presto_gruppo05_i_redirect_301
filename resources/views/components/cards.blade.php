498e0a62c0e97521f9e2da2128a11bc54c983a68
EliaBergamelli <eliabergamelli@Elias-MBP.fritz.box>
2023-05-11 14:09:26 +0200

    added connection between latest announcements and cards

--- resources/views/components/cards.blade.php
+++ resources/views/components/cards.blade.php
@@ -4,14 +4,13 @@
       <div class="card productCard" style="width: 18rem;">
         <img src="https://picsum.photos/200" class="card-img-top p-3 rounded" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-          <a href="#" class="btn btn-primary shadow">Go somewhere</a><a href="#"
+          <h5 class="card-title">{{$latestAnnouncement->title}}</h5>
+          <p class="card-text">{{$latestAnnouncement->price}}</p>
+          <p class="card-text">{{$latestAnnouncement->body}}</p>
+          <a href="#" class="btn btn-primary shadow">{{$latestAnnouncement->category->name}}</a><a href="#"
             class="my-2 border-top pt-2 border-dark card-link shadow btn btn-success">Go somewhere</a>
-          <p>Pubblicato il:</p>
+          <p>Pubblicato il: {{$latestAnnouncement->created_at}}</p>
+          <p>Caricato da: {{$latestAnnouncement->user}}</p>
         </div>
       </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,12 @@
 
     <div class="w-100">
         @foreach ($categories as $category)
-            
+            {{$category}}
+        @endforeach
+    </div>
+    <div class="w-100">
+        @foreach ($latestAnnouncements as $latestAnnouncement)
+            <x-cards :latestAnnouncement="$latestAnnouncement"></x-cards>
         @endforeach
     </div>
 
