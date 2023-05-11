0222640e4e88d41268b10076953a04e0db21516a
max5oooooo <maxuel5oooooo@gmail.com>
2023-05-11 20:07:40 +0200

    create announcements details

---
+++ resources/views/announcements/details.blade.php
@@ -0,0 +1,28 @@
+<x-main>
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-5 col-md-5 col-sm-6">
+            <div class="white-box text-center mt-5"><img src="https://picsum.photos/400" class="img-fluid rounded-3"></div>
+          </div>
+          <div class="col-lg-7 col-md-7 col-sm-6">
+            <h5 class="box-title mt-5 m-2">Titolo categoria</h5>
+            <h2 class="m-2 mb-4">Annuncio titolo</h2>
+            <div class="col-lg-7 col-12 bg-presto p-3 m-1 rounded-3">
+              <h2>9,69€(prezzo)</H2>
+                <h3>IVA ESCLUSA</h3>
+                <p>11,71€Pz IVA inclusa</p>
+            </div>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem doloremque placeat voluptatum. Reprehenderit, illum. Eius, itaque corrupti sed obcaecati necessitatibus magnam, exercitationem iste ad consequuntur excepturi aperiam numquam similique architecto.
+              </p>
+            </div>
+            <hr>
+            <p>Pubblicato il:</p>
+            <button type="submit" class="btn btn-success w-100 m-1">Registrati
+            </button>
+            <button type="submit" class="btn btn-success w-100 m-1">Registrati
+            </button>
+          </div>
+        </div>
+</x-main>
