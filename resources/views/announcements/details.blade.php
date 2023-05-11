8f4988d6a4b6c468b10223ef6ae5a9d780607ab3
max5oooooo <maxuel5oooooo@gmail.com>
2023-05-11 13:19:04 +0200

    added new card and details card

---
+++ resources/views/announcements/details.blade.php
@@ -0,0 +1,42 @@
+<x-main>
+    <header class="pt-5 pb-5  align-items-center d-flex bg-dark">
+        <div class="container ">
+          <div class="row align-items-center d-flex justify-content-between">
+            <div class="col-12  pb-5 order-2 order-sm-2 ">
+              <h1 class=" mb-3 mt-5 display-3 text-white">Nome categoria</h1>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div class="container">
+        <div class="card mt-3">
+          <div class="card-body">
+            <h3 class="card-title text-center">Titolo</h3>
+            <div class="row">
+              <div class="col-lg-5 col-md-5 col-sm-6">
+                <div class="white-box text-center"><img src="https://picsum.photos/400" class="img-fluid"></div>
+              </div>
+              <div class="col-lg-7 col-md-7 col-sm-6">
+                <h4 class="box-title mt-5">descrizione prodotto</h4>
+                <p>Lorem Ipsum available,but the majority have suffered alteration in some form,by injected humour,or
+                  randomised words which don't look even slightly believable.but the majority have suffered alteration in
+                  some form,by injected humour</p>
+                <h2 class="mt-5">
+                  125<small class="text-success">(50% di sconto)</small>
+                </h2>
+                <button class="btn btn-dark btn-rounded mr-1" data-toggle="tooltip" title=""
+                  data-original-title="Add to cart">
+                  <i class="fa fa-shopping-cart"></i>
+                </button>
+                <button class="btn btn-primary btn-rounded">Compra ora!</button>
+                <h3 class="box-title mt-5">Prova</h3>
+                <ul class="list-unstyled">
+                  <li><i class="fa fa-check text-success"></i>Prova</li>
+                  <li><i class="fa fa-check text-success"></i>Prova2</li>
+                  <li><i class="fa fa-check text-success"></i>Prova3</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+</x-main>
---
+++ resources/views/components/cards.blade.php
@@ -0,0 +1,19 @@
+<div class="container">
+  <div class="row">
+    <div class="col-12 col-md-4 my4 pt-5">
+      <div class="card productCard" style="width: 18rem;">
+        <img src="https://picsum.photos/200" class="card-img-top p-3 rounded" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <a href="#" class="btn btn-primary shadow">Go somewhere</a><a href="#"
+            class="my-2 border-top pt-2 border-dark card-link shadow btn btn-success">Go somewhere</a>
+          <p>Pubblicato il:</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
