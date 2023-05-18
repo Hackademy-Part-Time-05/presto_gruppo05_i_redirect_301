c956f25ec14b9694237bad1f6fdb2e7845c3bcd7
max5oooooo <maxuel5oooooo@gmail.com>
2023-05-18 20:57:02 +0200

    refresh view create announcement

--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -1,16 +1,11 @@
-<div>
-<div class="container-fluid hero-header bg-grey py-5 mb-5">
-  <div class="container py-5">
-      <div class="row g-5 align-items-center">
-          <div class="col-12">
-              <h1 class="display-4 mb-3 text-center">Benvenuto su presto.it!</h1>
-          </div>
-      </div>
+<section class="position-relative w-100 latest_announcements_section">
+  <div class="custom-shape-divider-bottom-1684350379">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+      </svg>
   </div>
-</div>
-  <section class=" mt-5">
-    <div class="container h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
+  <div class=" container cards_landscape_wrap-2 w-100 pb-3 mb-5">
+      <div class="row align-content-start justify-content-center g-4">
         <div class="col-lg-12 col-xl-11">
           <div class="card text-black" style="border-radius: 25px;">
             <div class="card-body p-md-5">
@@ -84,5 +79,10 @@
         </div>
       </div>
     </div>
-  </section>
+    <div class="custom-shape-divider-top-1684351062">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+      </svg>
+  </div>
+</section>
 </div>
