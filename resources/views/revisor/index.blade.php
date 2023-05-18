143b64f72b5dc1c60d4754b39ed3e2ed06595b56
max5oooooo <maxuel5oooooo@gmail.com>
2023-05-18 20:40:30 +0200

    renew all view

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -1,18 +1,23 @@
 <x-main>
-    <header class="pt-5 pb-5  align-items-center d-flex bg-dark">
-        <div class="container ">
-          <div class="row align-items-center d-flex justify-content-between">
-            <div class="col-12  pb-5 order-2 order-sm-2 ">
-              <h1 class=" mb-3 mt-5 display-3 text-white">Pagina di revisione</h1>
+  <div class="container-fluid hero-header py-5 mb-5">
+    <div class="container py-5">
+        <div class="row g-5 align-items-center">
+            <div class="col-12">
+                <h1 class="display-4 mb-3 animated slideInDown fw-bold text-center">Dashboard Revisore</h1>
             </div>
-          </div>
         </div>
-      </header>
+    </div>
+  </div>
+  <section class="position-relative w-100 latest_announcements_section">
+    <div class="custom-shape-divider-bottom-1684350379">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+        </svg>
+    </div>
       @if ($announcement_to_check)
-      <div class="container mt-5 mb-5 ">
-        <div class="row">         
+      <div class=" container cards_landscape_wrap-2 w-100 pb-3 mb-5">
+        <div class="row align-content-start justify-content-center g-4">
           <x-carousel></x-carousel>
-
           <div class="col-lg-6 col-md-6 col-sm-6">
             <h5 class="col-lg-7 col-md-7 col-sm-6 mt-5">{{$announcement_to_check->category->name}}</h5>
             <h2 class="col-lg-7 col-md-7 col-sm-6">{{$announcement_to_check->title}}</h2>
@@ -48,5 +53,15 @@
           </div>
         </div>
       </div>
+      @else
+      <div class="col-12">
+        <p class="h1 text-center">Non sono presenti annunci da revisionare!</p>
+      </div>
       @endif
+      <div class="custom-shape-divider-top-1684351062">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+        </svg>
+    </div>
+</section>
 </x-main>
