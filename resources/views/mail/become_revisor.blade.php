6b7d97f225e321351de554a2cea20369bd77a10b
max5oooooo <maxuel5oooooo@gmail.com>
2023-05-19 19:40:36 +0200

    renew view mail become revisor

--- resources/views/mail/become_revisor.blade.php
+++ resources/views/mail/become_revisor.blade.php
@@ -5,16 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Presto.it</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.19/jquery.touchSwipe.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    @livewireStyles
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    <div>
+<body>  
+    <section class="position-relative w-100 latest_announcements_section">
+    <div class="custom-shape-divider-bottom-1684350379">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+        </svg>
+    </div>
+    <div class=" container cards_landscape_wrap-2 w-100 pb-3 mb-5">
+        <div class="row align-content-start justify-content-center g-4">
         <h1>Un utente ha richiesto di lavorare con noi</h1>
         <h2>Ecco i suoi dati:</h2>
         <p>Nome: {{$user->name}}</p>
         <p>Email: {{$user->email}}</p>
         <p>se vuoi renderlo revisore clicca qui:</p>
-        <a href="{{route('make.revisor', compact('user'))}}">Rendi revisore</a>
+        <a href="{{route('make.revisor', compact('user'))}}" class="btn btn_main btn_categories">Rendi revisore</a>
+        </div>    
     </div>
-    
+    <div class="custom-shape-divider-top-1684351062">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+        </svg>
+    </div>
+</section>
 </body>
 </html>
