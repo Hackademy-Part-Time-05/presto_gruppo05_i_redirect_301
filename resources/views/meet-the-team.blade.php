e84c7bb17380a77239561bd123260428dca2cc1d
Andrea Luccadi <andrea.gnu.spotify@gmail.com>
2023-05-30 20:18:01 +0200

    i nostri creator

--- resources/views/meet-the-team.blade.php
+++ resources/views/meet-the-team.blade.php
@@ -1,3 +1,67 @@
 <x-main>
-    
+    <section id="team">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                  <h2>Il nostro Team</h2>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="https://media.licdn.com/dms/image/D4D35AQE72uPACxZ_ug/profile-framedphoto-shrink_800_800/0/1682090432645?e=1686074400&v=beta&t=mEj_hfaKDuX-nYKBm7XTLjIyh1DVWbG6El7gZGC-S7M" class="img-fluid" alt="">
+                      </div>
+                        <div class="member-info">
+                            <h4>Mattia Pastafiglia</h4>
+                            <span>Full stack developer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="https://media.licdn.com/dms/image/D4D35AQE8shkou8xN9g/profile-framedphoto-shrink_800_800/0/1684152611090?e=1686074400&v=beta&t=b2ODQVfkuM-NwMMbPZLBGKXg9c0UTZy7GqDB07GA-Jk" class="img-fluid" alt="">
+                      </div>
+                        <div class="member-info">
+                            <h4>Elia Bergamelli</h4>
+                            <span>Full stack developer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="https://media.licdn.com/dms/image/D4D35AQH8PS84vaVB2A/profile-framedphoto-shrink_400_400/0/1685470495880?e=1686078000&v=beta&t=ip_PGx3JuCBLGq7gUJ2-lIxbD-yzvw3KW7T4PeywLjA" class="img-fluid" alt="">
+                      </div>
+                        <div class="member-info">
+                            <h4>Andrea Luccadi</h4>
+                            <span>Full stack developer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </x-main>
