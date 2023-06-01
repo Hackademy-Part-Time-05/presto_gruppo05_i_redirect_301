0737a38c79c799b100bb236498a299a1e5479379
EliaBergamelli <eliabergamelli@192.168.1.10>
2023-06-01 21:46:16 +0200

    added borndate on fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
         'facebook',
         'instagram',
         'profession',
-        'birthDate',
+        'bornDate',
         'region',
         'city'
     ];
