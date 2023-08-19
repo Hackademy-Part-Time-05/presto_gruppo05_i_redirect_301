c18f436c30d66b12766d399fefe1ac04bb2c56c3
EliaBergamelli <eliabergamelli@Elias-MBP.fritz.box>
2023-08-19 16:36:27 +0200

    modify for resources

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -37,7 +37,7 @@ class ResizeImage implements ShouldQueue
         $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
         $croppedImage = Image::load($srcPath)
                         ->crop(Manipulations::CROP_CENTER , $w , $h)
-                        ->watermark('resources/img/Presto_logo_Main.png')
+                        ->watermark('/resources/img/Presto_logo_Main.png')
                         ->watermarkPosition(Manipulations::POSITION_BOTTOM_RIGHT)
                         ->watermarkHeight(20, Manipulations::UNIT_PERCENT)
                         ->watermarkWidth(20, Manipulations::UNIT_PERCENT)
